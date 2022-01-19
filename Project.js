4d43c7360564b02bb2243de7054128ba67731180
AleksMarinova <aleksandra.d.marinova@gmail.com>
2022-01-19 12:24:27 +0100

    changed schema

--- Project.js
+++ Project.js
@@ -6,6 +6,10 @@ const ProjectSchema = new Schema({
         type: String,
         required: true
     },
+    imageUrl:{
+        type: String,
+        required: true
+    },
     description: {  
         type: String,
         required: true
